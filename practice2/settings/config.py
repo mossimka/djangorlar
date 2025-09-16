05f1886e3d44c4847e71e2c0dcaddb6304327924
mossimka <maksimsarsekeev@gmail.com>
2025-09-16 14:07:23 +0500

    second bro

---
+++ practice2/settings/config.py
@@ -0,0 +1,7 @@
+# project modules
+from decouple import config
+
+# -----------------------------------------------------------------
+# variables
+ENV_ID 
+SECRET_KEY = 'django-insecure-i^f%hjuqb*qfkd86ux-a1is@(%i!)!lr7znzten#6q0_488)4v'
